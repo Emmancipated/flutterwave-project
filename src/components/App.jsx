2abf5ae7ec41506690d558bbaf13eb1e516f3f53
emmancipationera@gmail.com <macbook@Emmanuel-Kalu.local>
2022-12-18 11:12:10 +0100

    still on basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
    render() {
       return (
          <>
-         <Router basename="/flutterwave-project/">
+         <Router basename="/">
            <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/posts/:id" element={<Post />} />
